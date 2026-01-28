3d2acd20afdf13560cfb6880e692b5e99ae4af8f
Atahan Cesur <atahancesurr@gmail.com>
2026-01-28 17:56:25 +0300

    Update Eye Tracking Full Project.py

--- Eye Tracking Full Project.py
+++ Eye Tracking Full Project.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
+# In[1]:
 
 
 !pip install mediapipe opencv-python
@@ -12,7 +9,7 @@ import cv2
 print("MediaPipe başarıyla yüklendi!")
 print("OpenCV versiyon:", cv2.__version__)
 
-# In[1]:
+# In[2]:
 
 
 import cv2
